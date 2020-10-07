af83423319359ee0a385f46b7f1bf395a075f76c
Sølve Tornøe <solve@kvalitetskontroll.no>
2020-10-07 14:40:17 +0200

    Rename valueKey & rightAlign, change rightAlign api (#28)

--- src/docs/TableDocs.vue
+++ src/docs/TableDocs.vue
@@ -30,7 +30,7 @@
 
 <script>
 // eslint-disable-next-line no-useless-escape
-const basicExample = '<template>\n  <Table\n    :items=\"items\"\n    :headers=\"headers\"\n  \/>\n<\/template>\n\n<script>\nexport default {\n  name: \'TableExample\',\n  data: () => ({\n    headers: [\n      {\n        label: \'Label1\',\n        valueKey: \'Value1\',\n        rightAlign: false,\n      },\n      {\n        label: \'Label2\',\n        valueKey: \'Value2\',\n        rightAlign: false,\n      },\n      {\n        label: \'Label3\',\n        valueKey: \'Value3\',\n        rightAlign: false,\n      },\n      {\n        label: \'Label4\',\n        valueKey: \'Value4\',\n        rightAlign: false,\n      },\n    ],\n    items: [\n      {\n        Value1: \'Value1\',\n        Value2: \'Value2\',\n        Value3: \'Value3\',\n        Value4: \'Value4\',\n      },\n      {\n        Value1: \'Value1\',\n        Value2: \'Value2\',\n        Value3: \'Value3\',\n        Value4: \'Value4\',\n      },\n      {\n        Value1: \'Value1\',\n        Value2: \'Value2\',\n        Value3: \'Value3\',\n        Value4: \'Value4\',\n      },\n      {\n        Value1: \'Value1\',\n        Value2: \'Value2\',\n        Value3: \'Value3\',\n        Value4: \'Value4\',\n      },\n    ],\n  }),\n};\n<\/script>';
+const basicExample = '<template>\n  <Table\n    :items=\"items\"\n    :headers=\"headers\"\n  \/>\n<\/template>\n\n<script>\nexport default {\n  name: \'TableExample\',\n  data: () => ({\n    headers: [\n      {\n        label: \'Label1\',\n        value: \'Value1\',\n        },\n      {\n        label: \'Label2\',\n        value: \'Value2\',\n        },\n      {\n        label: \'Label3\',\n        value: \'Value3\',\n        },\n      {\n        label: \'Label4\',\n        value: \'Value4\',\n        align: \'right\',\n      },\n    ],\n    items: [\n      {\n        Value1: \'Value1\',\n        Value2: \'Value2\',\n        Value3: \'Value3\',\n        Value4: \'Value4\',\n      },\n      {\n        Value1: \'Value1\',\n        Value2: \'Value2\',\n        Value3: \'Value3\',\n        Value4: \'Value4\',\n      },\n      {\n        Value1: \'Value1\',\n        Value2: \'Value2\',\n        Value3: \'Value3\',\n        Value4: \'Value4\',\n      },\n      {\n        Value1: \'Value1\',\n        Value2: \'Value2\',\n        Value3: \'Value3\',\n        Value4: \'Value4\',\n      },\n    ],\n  }),\n};\n<\/script>';
 
 export default {
   name: 'TableDocs',
@@ -38,23 +38,20 @@ export default {
     headers: [
       {
         label: 'Label1',
-        valueKey: 'Value1',
-        rightAlign: true,
+        value: 'Value1',
       },
       {
         label: 'Label2',
-        valueKey: 'Value2',
-        rightAlign: false,
+        value: 'Value2',
       },
       {
         label: 'Label3',
-        valueKey: 'Value3',
-        rightAlign: false,
+        value: 'Value3',
       },
       {
         label: 'Label4',
-        valueKey: 'Value4',
-        rightAlign: false,
+        value: 'Value4',
+        align: 'right',
       },
     ],
     items: [
@@ -87,7 +84,7 @@ export default {
       {
         name: 'Headers',
         type: 'Array',
-        description: 'Array of header objects {label: x, valueKey: x, rightAlign: x}',
+        description: 'Array of header objects {label: x, value: x, align: x}',
       },
       {
         name: 'Items',
